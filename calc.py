1e41108ba084d51fb097c090cfa223a20b93f5b0
Student <student@example.com>
2025-08-19 19:15:32 +0000

    Added multiplication function and tested it

--- calc.py
+++ calc.py
@@ -5,6 +5,10 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
 print("Calculator started!")
 result = add(5, 3)
 print(f"5 + 3 = {result}")
+print(multiply(11*88))
